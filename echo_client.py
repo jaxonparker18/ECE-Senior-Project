8fc76393a8f87fa514c79eb1798f7efe5abcc086
Nathan Ray <71066202+nathan-ray@users.noreply.github.com>
2024-03-01 21:02:07 -0700

    UI update

--- echo_client.py
+++ echo_client.py
@@ -2,11 +2,13 @@
 
 import sys
 from socket import *
-import datetime
+
 from threading import Thread
+
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
+from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 
 HOST = "localhost"  # The server's hostname or IP address
@@ -16,6 +18,14 @@ class MainWindow(QWidget):
 
     def __init__(self):
         super(MainWindow, self).__init__()
+
+        self.status_label = None
+        # self.setStyleSheet("background-color: #202124;")    # for dark mode
+        self.setWindowTitle("RFES Control Center")
+        screen = QDesktopWidget().screenGeometry()
+        self.setGeometry(0, 0, screen.width(), screen.height())
+        self.showMaximized()
+
         self.ip_entry = QLineEdit()
         self.port_entry = QLineEdit()
         self.c_d_button = QPushButton()
@@ -28,8 +38,8 @@ class MainWindow(QWidget):
         self.defaultPort = "2100"
 
         self.info = QLabel()
-        self.setWindowTitle("RFES Control Center")
-        self.setGeometry(50, 50, 320, 200)
+        self.info.setFont(QFont("sans serif", 12))
+
 
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
@@ -39,24 +49,34 @@ class MainWindow(QWidget):
         self.top_widget.setLayout(self.top_panel_layout)
 
         self.TL_widget = QWidget()
-        self.TL_layout = QVBoxLayout()
+        self.TL_layout = QHBoxLayout()
+        self.TL_layout.setAlignment(Qt.AlignLeft)
         self.TL_widget.setLayout(self.TL_layout)
 
         self.TR_widget = QWidget()
         self.TR_layout = QVBoxLayout()
-        self.TR_layout.setAlignment(Qt.AlignCenter)
+        self.TR_layout.setAlignment(Qt.AlignRight)
         self.TR_widget.setLayout(self.TR_layout)
 
         self.top_panel_layout.addWidget(self.TL_widget)
         self.top_panel_layout.addWidget(self.TR_widget)
 
         self.layout.addWidget(self.top_widget)
+
+        self.bot_widget = QWidget()
+        self.bot_layout = QHBoxLayout()
+        self.bot_widget.setLayout(self.bot_layout)
+
+        self.layout.addWidget(self.bot_widget)
+
         self.status = "DISCONNECTED"
 
-        self.create_ip_panel()
-        self.create_port_panel()
+        self.create_label_panel()
+        self.create_entry_panel()
         self.create_connect_disconnect_panel()
         self.show_status()
+
+        self.display_web()
 
     def server_connection(self):
         while self.status == "CONNECTED":
@@ -88,6 +108,18 @@ class MainWindow(QWidget):
             self.update_status()
             self.socket.close()
 
+    def display_web(self):
+        browser = QWebEngineView()
+        browser.setFixedSize(1500, 1000)
+
+        browser.setUrl(QUrl("http://google.com"))
+        self.bot_layout.addWidget(browser)
+
+    # def show_logs(self):
+    #     log_widget = QWidget()
+    #     log_layout = Q
+
+
     def keyPressEvent(self, event):
         if not event.isAutoRepeat():
             if event.text() == 'w':
@@ -114,55 +146,74 @@ class MainWindow(QWidget):
             if event.text() == ' ':
                 self.keys[4] = '0'
 
-    def create_ip_panel(self):
-        ip_panel_widget = QWidget()
-        ip_panel_layout = QHBoxLayout()
-        ip_panel_widget.setLayout(ip_panel_layout)
+    def create_label_panel(self):
+        TL_L_widget = QWidget()
+        TL_L_layout = QVBoxLayout()
+        TL_L_widget.setLayout(TL_L_layout)
 
         ip_label = QLabel("IP Address:")
-        ip_panel_layout.addWidget(ip_label)
-
-        self.ip_entry = QLineEdit()
-        self.ip_entry.setText(self.defaultIP)
-        ip_panel_layout.addWidget(self.ip_entry)
-
-        self.TL_layout.addWidget(ip_panel_widget)
-
-
-
-    def create_port_panel(self):
-        port_panel_widget = QWidget()
-        port_panel_layout = QHBoxLayout()
-        port_panel_widget.setLayout(port_panel_layout)
+        ip_label.setFont(QFont("sans serif", 15))
+        ip_label.setFixedSize(120, 30)
+        TL_L_layout.addWidget(ip_label)
 
         port_label = QLabel("Port:")
-        port_panel_layout.addWidget(port_label)
+        port_label.setFont(QFont("sans serif", 15))
+        port_label.setFixedSize(40, 30)
+        TL_L_layout.addWidget(port_label)
+
+        self.TL_layout.addWidget(TL_L_widget)
+
+
+    def create_entry_panel(self):
+        TL_R_widget = QWidget()
+        TL_R_layout = QVBoxLayout()
+        TL_R_widget.setLayout(TL_R_layout)
+
+        self.ip_entry = QLineEdit()
+        self.ip_entry.setFixedSize(200, 25)
+        self.ip_entry.setText(self.defaultIP)
+        self.ip_entry.setFont(QFont("sans serif", 12))
+        TL_R_layout.addWidget(self.ip_entry)
 
         self.port_entry = QLineEdit()
+        self.port_entry.setFixedSize(80, 25)
         self.port_entry.setText(self.defaultPort)
-        port_panel_layout.addWidget(self.port_entry)
+        self.port_entry.setFont(QFont("sans serif", 12))
+        TL_R_layout.addWidget(self.port_entry)
 
-        self.TL_layout.addWidget(port_panel_widget)
+        self.TL_layout.addWidget(TL_R_widget)
 
     def create_connect_disconnect_panel(self):
-        c_d_widget = QWidget()
-        c_d_layout = QHBoxLayout()
-        c_d_widget.setLayout(c_d_layout)
+        button_widget = QWidget()
+        button_layout = QVBoxLayout()
+        button_widget.setLayout(button_layout)
+        # button_layout.setAlignment(Qt.AlignLeft)
+
+        left_spacer = QSpacerItem(0, 1)
+        button_layout.addItem(left_spacer)
 
         self.c_d_button.setText("Connect")
+        self.c_d_button.setFont(QFont("sans serif", 12))
+        self.c_d_button.setFixedSize(200, 50)
         self.c_d_button.clicked.connect(self.connect_to_server)
+        button_layout.addWidget(self.c_d_button)
 
-        c_d_layout.addWidget(self.c_d_button)
-        self.TL_layout.addWidget(c_d_widget)
+        right_spacer = QSpacerItem(800, 1)
+        button_layout.addItem(right_spacer)
+
+        self.TL_layout.addWidget(button_widget)
 
     def show_status(self):
         self.status_label = QLabel()
-        font = QFont("sans serif", 12)
+        font = QFont("sans serif", 20)
         font.setBold(True)
         self.status_label.setFont(font)
-        self.info.hide()
+        self.status_label.setFixedSize(220, 100)
         self.status_label.setText(self.status)
         self.status_label.setStyleSheet("color:red")
+
+        self.info.setFixedSize(400, 20)
+        self.info.hide()
 
         self.TR_layout.addWidget(self.info)
         self.TR_layout.addWidget(self.status_label)
@@ -173,6 +224,7 @@ class MainWindow(QWidget):
             self.status_label.setText(self.status)
             self.status_label.setStyleSheet("color:red")
         else:
+            self.info.hide()
             self.c_d_button.setText("Disconnect")
             self.status_label.setText(self.status)
             self.status_label.setStyleSheet("color:green")
