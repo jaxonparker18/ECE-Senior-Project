b56a8dadd203e4e6104f07da7d2cac7c964f7924
Nathan Ray <71066202+nathan-ray@users.noreply.github.com>
2024-03-09 22:18:20 -0700

    updated bugs and improved GUI

--- echo_client.py
+++ echo_client.py
@@ -1,5 +1,5 @@
 # echo-client.py
-
+from datetime import datetime
 import sys
 from socket import *
 
@@ -14,16 +14,20 @@ from PyQt5.QtWebEngineWidgets import *
 HOST = "localhost"  # The server's hostname or IP address
 PORT = 0  # The port used by the server, must be >= 1024
 
+client = 0
+server = 1
+
 
 class WorkerThread(QThread):
     update_signal = pyqtSignal(str)
 
-    def __init__(self, parent, message):
+    def __init__(self, parent, side, message):
         super().__init__(parent)
+        self.side = side
         self.message = message
 
     def run(self):
-        self.update_signal.emit(self.message)
+        self.update_signal.emit(self.side, self.message)
 
 
 class MainWindow(QWidget):
@@ -43,6 +47,8 @@ class MainWindow(QWidget):
         self.port_entry = QLineEdit()
         self.c_d_button = QPushButton()
         self.socket = None
+        self.browser = None
+        self.no_feed = None
 
         # corresponds to W, A, S, D, spacebar
         self.keys = ['0', '0', '0', '0', '0']
@@ -50,9 +56,6 @@ class MainWindow(QWidget):
         # default values
         self.defaultIP = "localhost"
         self.defaultPort = "2100"
-
-        self.info = QLabel()
-        self.info.setFont(QFont("sans serif", 12))
 
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
@@ -92,28 +95,31 @@ class MainWindow(QWidget):
         self.create_connect_disconnect_panel()
 
     def server_connection(self):
+
         while self.status == "CONNECTED":
             try:
                 self.socket.sendall((''.join(self.keys)).encode('utf-8'))
-                # worker_thread = WorkerThread(self, )
-                # worker_thread.update_signal.connect(self.log)
-                # worker_thread.start()
+
             except:
                 self.status = "DISCONNECTED"
-                self.update_status()
+                worker_thread = WorkerThread(self, server, "Connection with server closed.")
+                worker_thread.update_signal.connect(self.log)
+                # worker_thread.update_signal.connect(self.update_status())
+                worker_thread.start()
+                # self.log(client, "Connection with server closed.")
+                # self.update_status()
                 break
-            # self.log("testt")      # "being sent: " + ''.join(self.keys)
-
 
     def connect_to_server(self):
         self.c_d_button.clearFocus()
         if self.status == "DISCONNECTED":
+            self.log(client, "Connecting to server...")
             self.socket = socket(AF_INET, SOCK_STREAM)
             try:
                 self.socket.connect((self.ip_entry.text(), int(self.port_entry.text())))
+                self.log(server, self.socket.recv(1024).decode('utf-8'))
             except:
-                self.info.setText("Connection with RFES cannot be established. Try again.")
-                self.info.show()
+                self.log(client, "Connection with RFES cannot be established. Try again.")
                 return
             self.status = "CONNECTED"
             self.update_status()
@@ -121,18 +127,20 @@ class MainWindow(QWidget):
             thread.start()
         else:
             self.status = "DISCONNECTED"
+            self.log(client, "Connection closed.")
             self.update_status()
             self.socket.close()
 
     def display_web(self):
-        # browser = QWebEngineView()
-        # browser.setFixedSize(1500, 840)
-        # browser.setUrl(QUrl("http://google.com"))
-        # self.bot_layout.addWidget(browser)
-        sample_pic = QLabel()
-        sample_pic.setFixedSize(1500, 840)
-        sample_pic.setPixmap(QPixmap("samplefeed.jpg"))
-        self.bot_layout.addWidget(sample_pic)
+        self.browser = QWebEngineView()
+        self.browser.setFixedSize(1500, 840)
+        self.browser.setUrl(QUrl("http://google.com"))
+
+        self.no_feed = QLabel()
+        self.no_feed.setFixedSize(1500, 840)
+        self.no_feed.setPixmap(QPixmap("not_connected.png"))
+        self.no_feed.setAlignment(Qt.AlignCenter)
+        self.bot_layout.addWidget(self.no_feed)
 
     def display_logs(self):
         logs_w = QWidget()
@@ -143,25 +151,33 @@ class MainWindow(QWidget):
         logger_label.setFont(QFont("sans serif", 12))
         logs_l.addWidget(logger_label)
 
-        self.logger = QTextEdit()
+        self.logger = QTextBrowser(self)
         self.logger.setFixedSize(330, 800)
         self.logger.setReadOnly(True)
+        self.logger.setFocusPolicy(Qt.NoFocus)
 
-        self.logger.setAlignment(Qt.AlignBottom)
-        self.logger.setText("TEST")
+        self.logger.setAlignment(Qt.AlignJustify)
+        self.logger.setText("Welcome to RFES Control Center.")
         self.logger.setContentsMargins(5, 0, 0, 5)
         logs_l.addWidget(self.logger)
 
         self.bot_layout.addWidget(logs_w)
 
-    def log(self, message):
-        self.logger.setText(str(self.logger.toPlainText()) + "\n" + "CLIENT: " + message)
+    def log(self, side, message):
+        time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        if side == 0:
+            self.logger.setText(str(self.logger.toPlainText()) + "\n" + time + " - CLIENT: " + message)
+        else:
+            self.logger.setText(str(self.logger.toPlainText()) + "\n" + time + " - SERVER: " + message)
+        self.logger.verticalScrollBar().setValue(
+            self.logger.verticalScrollBar().maximum()
+        )
 
     def keyPressEvent(self, event):
         if not event.isAutoRepeat():
             if event.text() == 'w':
                 self.keys[0] = '1'
-                self.log("Moving forward.")
+                self.log(client, "Moving forward.")
             if event.text() == 'a':
                 self.keys[1] = '1'
             if event.text() == 's':
@@ -175,7 +191,7 @@ class MainWindow(QWidget):
         if not event.isAutoRepeat():
             if event.text() == 'w':
                 self.keys[0] = '0'
-                self.log("Stopped moving forward.")
+                self.log(client, "Stopped moving forward.")
             if event.text() == 'a':
                 self.keys[1] = '0'
             if event.text() == 's':
@@ -253,22 +269,34 @@ class MainWindow(QWidget):
         self.status_label.setText(self.status)
         self.status_label.setStyleSheet("color:red")
 
-        self.info.setFixedSize(400, 20)
-        self.info.hide()
-
-        self.TR_layout.addWidget(self.info)
         self.TR_layout.addWidget(self.status_label)
 
     def update_status(self):
         if self.status == "DISCONNECTED":
+            self.ip_entry.setReadOnly(False)
+            self.port_entry.setReadOnly(False)
             self.c_d_button.setText("Connect")
             self.status_label.setText(self.status)
             self.status_label.setStyleSheet("color:red")
+
+            # GUI update
+            self.bot_layout.removeWidget(self.browser)
+            self.browser.setParent(None)
+            self.bot_layout.insertWidget(0, self.no_feed)
+            self.update()
+
         else:
-            self.info.hide()
+            self.ip_entry.setReadOnly(True)
+            self.port_entry.setReadOnly(True)
             self.c_d_button.setText("Disconnect")
             self.status_label.setText(self.status)
             self.status_label.setStyleSheet("color:green")
+
+            self.bot_layout.removeWidget(self.no_feed)
+            self.no_feed.setParent(None)
+            self.bot_layout.insertWidget(0, self.browser)
+            self.update()
+
 
 
 
--- echo_server.py
+++ echo_server.py
@@ -1,12 +1,13 @@
 # echo-server.py
 
 from socket import *
+import sys
 import datetime
 
 
 def get_most_recent(data_bytes):
     string_data = data_bytes.decode('utf-8')
-    return string_data[len(string_data) - 5 : len(string_data)]
+    return string_data[len(string_data) - 5: len(string_data)]
 
 
 HOST = "localhost"  # Standard loopback interface address (localhost)
@@ -17,17 +18,26 @@ with socket(AF_INET, SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     while True:
-        conn, addr = s.accept()
-        print(f"Control center connected at {addr}.")
-        while True:
-            try:
-                data = conn.recv(1024)
-                if data:
-                    print("data is " + get_most_recent(data))
-                if not data:
+        try:
+            conn, addr = s.accept()
+            print(f"Control center connected at {addr}.")
+            conn.sendall((''.join("Connection established.")).encode('utf-8'))
+            while True:
+                try:
+                    data = conn.recv(1024)
+                    if data:
+                        print("data is " + get_most_recent(data))
+                    if not data:
+                        print("Disconnected from control center")
+                        break
+                except KeyboardInterrupt:
+                    print("Disconnected from control center")
+                    s.close()
+                    sys.exit(0)
+                except:
                     print("Disconnected from control center")
                     break
-            except:
-                print("Disconnected from control center")
-                break
-
+        except KeyboardInterrupt:
+            print("Server hard-stopped with CTRL + C.")
+            s.close()
+            sys.exit(0)
