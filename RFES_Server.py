4c0db0c44cec60a151c114af2892708d18c1568c
Nathan Ray <71066202+nathan-ray@users.noreply.github.com>
2024-03-14 12:31:17 -0600

    added little details

--- RFES_Server.py
+++ RFES_Server.py
@@ -1,18 +1,17 @@
 # echo-server.py
-
+import socket
 from socket import *
 import sys
 import datetime
 import numpy
-# from gpiozero import PWMLED
 from time import sleep
+
+# from gpiozero import PWMLED
+# led = PWMLED("BOARD32")
 
 HOST = "localhost"  # Standard loopback interface address (localhost)
 # Pi server = 172.20.10.3
 PORT = 2100  # Port to listen on (non-privileged ports are > 1023)
-
-# led = PWMLED("BOARD32")
-
 
 def get_most_recent(data_bytes):
     string_data = data_bytes.decode('utf-8')
@@ -24,20 +23,92 @@ def execute_commands(bits):
     if len(bits) != 9:
         return
 
-    if bits[0] == '1':
-        pass
+    execute_w(bits[0])
+    execute_a(bits[1])
+    execute_s(bits[2])
+    execute_d(bits[3])
+    execute_space(bits[4])
+    execute_up(bits[5])
+    execute_down(bits[6])
+    execute_left(bits[7])
+    execute_right(bits[8])
+
+
+def execute_w(cond):
+    if cond == '1':
+        print("forward")
+        # ramping up speed
+        for v in numpy.arange(0, 1, 0.001):
+            # led.value = v
+
         # led.value = 1
-        # print("forward")
+
+    else:
+        # led.value = 0
+        print("stop forward")
+
+
+def execute_a(cond):
+    if cond == '1':
+        pass
     else:
         pass
-        # led.value = 0
-        # print("stop")
+
+
+def execute_s(cond):
+    if cond == '1':
+        pass
+    else:
+        pass
+
+
+def execute_d(cond):
+    if cond == '1':
+        pass
+    else:
+        pass
+
+
+def execute_space(cond):
+    if cond == '1':
+        pass
+    else:
+        pass
+
+
+def execute_up(cond):
+    if cond == '1':
+        pass
+    else:
+        pass
+
+
+def execute_down(cond):
+    if cond == '1':
+        pass
+    else:
+        pass
+
+
+def execute_left(cond):
+    if cond == '1':
+        pass
+    else:
+        pass
+
+
+def execute_right(cond):
+    if cond == '1':
+        pass
+    else:
+        pass
 
 
 with socket(AF_INET, SOCK_STREAM) as s:
     s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen()
+    print("Server online.")
     while True:
         try:
             conn, addr = s.accept()
@@ -45,9 +116,9 @@ with socket(AF_INET, SOCK_STREAM) as s:
             conn.sendall((''.join("Connection established.")).encode('utf-8'))
             while True:
                 try:
-                    data = conn.recv(1024)
+                    data = conn.recv(9, MSG_WAITALL)
                     if data:
-                        print(data)
+                        # print(data)
                         # print("data is " + get_most_recent(data))
                         execute_commands(get_most_recent(data))
                     if not data:
