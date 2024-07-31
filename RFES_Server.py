3e8a977a6815bf653a6abeb403c726c5b513b097
Austin LaMontagne <lamontagneaustin@gmail.com>
2024-07-31 00:33:10 -0600

    Updating RFES to current repository. Also new Servo code.

--- RFES_Server.py
+++ RFES_Server.py
@@ -1,5 +1,6 @@
 # echo-server.py
 import sys
+
 sys.path.append('/usr/lib/python3/dist-packages')
 import threading
 from socket import *
@@ -12,136 +13,240 @@ import imutils
 import io
 # PI EXCLUSIVE
 import serial
-from gpiozero import PWMLED
+from gpiozero import Motor, Servo, PWMLED
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import FileOutput
 
+class Ser():
+    def __init__(self):
+        self.value = 0
+
+class UpdateServoThread(threading.Thread):
+    def __init__(self, servo, direction):
+        super().__init__()
+        self.servo = servo
+        self.direction = direction
+        self._stop_event = threading.Event()
+        self.delay = DELAY
+        self.new_val = 0
+
+    def run(self):
+        self.servo.on()
+        global curr_x
+        self.new_val = curr_x
+        while not self._stop_event.is_set():
+            if self.delay < DELAY:
+                self.delay += 1
+                continue
+            if self.direction == LEFT:
+                self.new_val += INCS
+            elif self.direction == RIGHT:
+                self.new_val -= INCS
+            elif self.direction == UP:
+                self.new_val += INCS
+            elif self.direction == DOWN:
+                self.new_val -= INCS
+            if self.new_val > 0.3:
+                self.new_val = 0.3
+            if self.new_val < 0.04:
+                self.new_val = 0.04
+            self.servo.value = self.new_val
+            curr_x = self.new_val
+            self.delay = 0
+            print("servo", curr_x)
+
+    def stop(self):
+        self.servo.off()
+        self._stop_event.set()
+
+# CONSTS
+INCS = 0.01
+
+DELAY = 1000000
+
+UP = 0
+DOWN = 1
+LEFT = 2
+RIGHT = 3
+
 # UART
-serial_port = '/dev/ttyAMA0'
+serial_port = '/dev/ttyAMA10'  # debug port -> '/dev/ttyAMA10', USB -> '/dev/USB0' | busted -> '/dev/ttyAMA0'
 baud_rate = 115200
 ser = serial.Serial(serial_port, baud_rate)
 
 # GPIO MOTOR
-left_motorA = PWMLED("BOARD11")
-left_motorB = PWMLED("BOARD13")
-right_motorA = PWMLED("BOARD16")
-right_motorB = PWMLED("BOARD18")
-left_motor = 0
-right_motor = 1
+left_motor = Motor("BOARD11", "BOARD13")
+right_motor = Motor("BOARD18", "BOARD16")
 
+# SERVOS
+x_servo = PWMLED("BOARD40")
+x_servo_thread = UpdateServoThread(x_servo, LEFT)
+curr_x = 0
+
+# SERVER
+client_socket = None
 
 def clean_up():
-    global left_motorA
-    global left_motorB
-    global right_motorA
-    global right_motorB
-    global ser
-    left_motorA.close()
-    left_motorB.close()
-    right_motorA.close()
-    right_motorB.close()
+    """
+    Closes any GPIO pins.
+    """
     ser.close()
 
 
 def get_most_recent(data_bytes):
+    """
+    Gets the most recent command from data bytes.
+    :param data_bytes: Bytes of data to be filtered
+    :return: most recent bytes of data, 9 characters long
+    """
     string_data = data_bytes.decode('utf-8')
     return string_data[len(string_data) - 9: len(string_data)]
 
+def send_to_client(data):
+    print("sent")
+    client_socket.sendall((''.join(data).encode('utf-8')))
 
-def set_motor(left, right):
-    global left_motorA
-    global left_motorB
-    global right_motorA
-    global right_motorB
-
-    if left == 0:
-        left_motorA.value = 0
-        left_motorB.value = 0
-    elif left > 0:
-        left_motorA.value = left
-        left_motorB.value = 0
+def set_motor(left_speed, right_speed):
+    """
+    Sets the left and right motor.
+    :param left: value of left motor (0 - 1), where positive is forward and negative is backward
+    :param right: value of right motor (0 - 1), where positive is forward and negative is backward
+    """
+    
+    global left_motor
+    global right_motor
+    
+    if left_speed == 0:
+        left_motor.stop()
+    elif left_speed > 0:
+        left_motor.forward(left_speed)
     else:
-        left_motorA.value = 0
-        left_motorB.value = abs(left)
+        left_motor.backward(abs(left_speed))
 
-    if right == 0:
-        right_motorA.value = 0
-        right_motorB.value = 0
-    elif right > 0:
-        right_motorA.value = right
-        right_motorB.value = 0
+    if right_speed == 0:
+        right_motor.stop()
+    elif right_speed > 0:
+        right_motor.forward(right_speed)
     else:
-        right_motorA.value = 0
-        right_motorB.value = abs(right)
+        right_motor.backward(abs(right_speed))
 
 
 def send_to_uart(bits):
+    """
+    Send bits to UART.
+    :param bits: bits to be sent
+    """
+
     for i in range(len(bits)):
         uart_command = str(bits[i]).encode('utf-8')
         ser.write(uart_command)
+    # print("sent to UART: " + bits)
 
 
 def execute_commands(bits):
-    print(bits)
-    if len(bits) != 9:
-        return
+    """
+    Execute the commands based on bits, where bits is [w, a, s, d, space, up, down, left, right]
+    :param bits: 9 characters long
+    """
+    try:
+        print(bits)
+        if len(bits) != 9:
+            return
 
-    w, a, s, d, space, up, down, left, right = bits
-    w, a, s, d, space, up, down, left, right = int(w), int(a), int(s), int(d), int(space), int(up), int(down), int(
-        left), int(right)
+        w, a, s, d, space, up, down, left, right = bits
+        w, a, s, d, space, up, down, left, right = int(w), int(a), int(s), int(d), int(space), int(up), int(down), int(
+            left), int(right)
+        
+        # MOVEMENT
+        if w and a:
+            set_motor(0.10, 0.75)
+            send_to_client("W and A executed.")
+        elif w and d:
+            set_motor(0.75, 0.10)
+            send_to_client("W and D executed.")
+        elif w and s:
+            set_motor(0, 0)
+            send_to_client("W and S executed.")
+        elif s and a:
+            set_motor(-0.25, -0.75)
+            send_to_client("S and A executed.")
+        elif s and d:
+            set_motor(-0.75, -0.25)
+            send_to_client("S and D executed.")
+        elif w:
+            set_motor(0.75, 0.75)
+            send_to_client("W executed.")
+        elif s:
+            set_motor(-0.75, -0.75)
+            send_to_client("S executed.")
+        elif a:
+            set_motor(-0.75, 0.75)
+            send_to_client("A executed.")
+        elif d:
+            set_motor(0.75, -0.75)
+            send_to_client("D executed.")
+        else:
+            set_motor(0, 0)
+            send_to_client("RFES on standby.")
 
-    # MOVEMENT
-    if w and a:
-        set_motor(0.5, 0.75)
-    elif w and d:
-        set_motor(0.75, 0.5)
-    elif w and s:
-        set_motor(0, 0)
-    elif s and a:
-        set_motor(-0.5, -0.75)
-    elif s and d:
-        set_motor(-0.75, -0.5)
-    elif w:
-        set_motor(0.75, 0.75)
-    elif s:
-        set_motor(-0.75, -0.75)
-    elif a:
-        set_motor(-0.75, 0.75)
-    elif d:
-        set_motor(0.75, -0.75)
-    else:
-        set_motor(0, 0)
+        # FIRING MECHANISM
+        # send_to_uart(bits[4:])
+        global x_servo_thread
+        if left:
+            if not x_servo_thread.is_alive():
+                x_servo_thread = UpdateServoThread(x_servo, LEFT)
+                x_servo_thread.start()
+                return
 
-    # FIRING MECHANISM
-    send_to_uart(bits[4:])
+        elif not left:
+            if x_servo_thread.is_alive():
+                x_servo_thread.stop()
+                x_servo_thread.join()
 
+        if right:
+            if not x_servo_thread.is_alive():
+                x_servo_thread = UpdateServoThread(x_servo, RIGHT)
+                x_servo_thread.start()
+                return
 
+        elif not right:
+            if x_servo_thread.is_alive():
+                x_servo_thread.stop()
+                x_servo_thread.join()
+                
+    except Exception as e:
+        send_to_client("ERROR OCCURED: " + repr(e))
+        
 def handle_commands():
+    """
+    Establishes handshake with TCP Client and listens to any commands sent from the client.
+    """
+    global client_socket
     while True:
         try:
             client_socket, tcp_address = commands_socket.accept()
-            print(f"Control center connected at {tcp_address}.")
-            client_socket.sendall((''.join("Connection established.")).encode('utf-8'))
+            print(f"Command center connected at {tcp_address}.")
+            send_to_client("Connection established.")
             while True:
                 try:
                     data = client_socket.recv(9, MSG_WAITALL)
                     if data:
                         # print(data)
-                        # print("data is " + get_most_recent(data))
+                        print("data is " + get_most_recent(data))
                         execute_commands(get_most_recent(data))
                     if not data:
-                        print("Disconnected from control center")
+                        print("Commands disconnected from control center: No data")
                         break
                 except KeyboardInterrupt:
-                    print("Disconnected from control center")
+                    print("Commands disconnected from control center: Keyboard Interrupt exception")
                     client_socket.close()
                     clean_up()
                     sys.exit(0)
-                except:
-                    print("Disconnected from control center")
-                    clean_up()
-                    break
+                #except:
+                #    print("Commands disconnected from control center: Failed to receive exception")
+                #    clean_up()
+                #    break
         except KeyboardInterrupt:
             print("Server hard-stopped with CTRL + C.")
             client_socket.close()
@@ -150,25 +255,37 @@ def handle_commands():
 
 
 def handle_video():
+    """
+    Starts the PiCamera and sends video feed from the camera to TCP client.
+    """
+
     picam2 = Picamera2()
     # (3280, 2646), (1920, 1080), (1640, 1232), (640, 480)
     picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (1640, 1232)}))
     picam2.start()
 
     while True:
-        client_sock, udp_address = video_socket.accept()
-        print(f"Video feed connected at {udp_address}.")
+        client_sock, tcp_address = video_socket.accept()
+        print(f"Video control connected at {tcp_address}.")
         while True:
             im = picam2.capture_array()
             encoded, buffer = cv2.imencode('.jpg', im, [cv2.IMWRITE_JPEG_QUALITY, 70])
             message = base64.b64encode(buffer) + b'\0'
-            print(len(message))
-            client_sock.sendto(message, udp_address)
+            try:
+                client_sock.sendto(message, tcp_address)
+            except KeyboardInterrupt:
+                print("Server hard-stopped with CTRL + C.")
+                client_sock.close()
+                clean_up()
+                sys.exit(0)
+            except:
+                print("Video control disconnected from control center")
+                break
 
 
 BUFF_SIZE = 65536
 
-HOST = "172.20.10.3"  # Standard loopback interface address (localhost)
+HOST = "10.42.0.1"  # Standard loopback interface address (localhost)
 # Pi server = 172.20.10.3 / 10.42.0.1
 
 # COMMANDS SOCKET
--- test.py
+++ test.py
@@ -1,13 +1,120 @@
-from gpiozero import Servo
-import numpy as np
+#!/usr/bin/python
+#--------------------------------------
+#    ___  ___  _ ____
+#   / _ \/ _ \(_) __/__  __ __
+#  / , _/ ___/ /\ \/ _ \/ // /
+# /_/|_/_/  /_/___/ .__/\_, /
+#                /_/   /___/
+#
+#           servo3.py
+# Example script to control a servo
+# connected to GPIO17 using the Gpiozero library.
+# Allows tweaking of pulse widths to get the full
+# range of movement.
+# Uses "value" to move between min and max rotation.
+#
+# Use CTRL-C to break out of While loop.
+#
+# Author : Matt Hawkins
+# Date   : 20/01/2018
+#
+# https://www.raspberrypi-spy.co.uk/tag/servo/
+#
+#--------------------------------------
+from gpiozero import Servo 
+from gpiozero import PWMLED
 from time import sleep
+import numpy
 
-servo = Servo("BOARD32")
+correctionMAX = 0.5
+correctionMIN = 0.5
+maxPW = (2.0 + correctionMAX)/1000
+minPW = (1.0 - correctionMIN)/1000
+
+led = Servo(17, min_pulse_width=minPW, max_pulse_width=maxPW)
+print(led.max_pulse_width)
+num = 0
 
 while True:
-    servo.min()
+
+    """
+    print("Set value range -1.0 to +1.0")
+    for value in range(0,21):
+        value2=(float(value)-10)/10
+        led.value=value2
+        print("expected:" + str(value2))
+        print("actual:" + str(led.value))
+        sleep(0.5)
+        
+    print("Set value range +1.0 to -1.0")
+    for value in range(20, -1, -1):
+        value2=(float(value)-10)/10
+        led.value=value2
+        print("expected:" + str(value2))
+        print("actual:" + str(led.value))
+        sleep(0.5)
+    """
+    
+    ## Increment Test for Servo
+    """
+    if num >= 1.0:
+        num = -1.0
+    led.value = num
+    num += 0.025
+    
+    print("expec: " + str(num))
+    print("actual: " + str(led.value))
+    sleep(0.05)
+    """
+    
+    ## Min. and Max. Test for Servo
+
+    led.min()
+    print("expec: " + str(num))
+    print("actual: " + str(led.value))
     sleep(1)
-    servo.mid()
+    led.max()
+    print("expec: " + str(num))
+    print("actual: " + str(led.value))
     sleep(1)
-    servo.max()
+    
+    ## Exact Value Test for Servo
+    """
+    led.value = -1.0
+    print("expec: " + str(-1.0))
+    print("actual: " + str(led.value))
     sleep(1)
+    led.value = -0.8
+    print("expec: " + str(-0.8))
+    print("actual: " + str(led.value))
+    sleep(1)
+    led.value = -0.4
+    print("expec: " + str(-0.4))
+    print("actual: " + str(led.value))
+    sleep(1)
+    led.value = 0.0
+    print("expec: " + str(0.0))
+    print("actual: " + str(led.value))
+    sleep(1)
+    led.value = 0.4
+    print("expec: " + str(0.4))
+    print("actual: " + str(led.value))
+    sleep(1)
+    led.value = 0.8
+    print("expec: " + str(0.8))
+    print("actual: " + str(led.value))
+    sleep(1)
+    led.value = 1.0
+    print("expec: " + str(1.0))
+    print("actual: " + str(led.value))
+    sleep(1)
+    """
+    
+    ## Nathan's Original Test Code
+    """
+    for duty_cycle in range(0, 100, 1):
+        led.value = duty_cycle/100.0
+        print("expec: " + str(duty_cycle/100.0))
+        print("actual: " + str(led.value))
+        sleep(0.1)
+    """
